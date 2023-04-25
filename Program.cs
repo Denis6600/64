b0b0d43d206e6538f34c3fa53fef61f18c517ebc
Denis Yalymov <>
2023-04-25 21:10:28 +0300

    64

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 64: Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void GetNubmer(int m, int n)
+{
+if (m > n) return;
+Console.Write($"{m}, ");
+GetNubmer(m+1,n);
+}
+GetNubmer(m,n);
